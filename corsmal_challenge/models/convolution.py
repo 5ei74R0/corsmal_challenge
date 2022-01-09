34e30f8aeb5212ea1e7fc11e9a1eddf759ea1323
OtsuKotsu <otoxukk.y@gmail.com>
2022-01-09 22:57:03 +0900

    Update. Add Inverted ResBlock like the one in MobileNetV2

--- corsmal_challenge/models/convolution.py
+++ corsmal_challenge/models/convolution.py
@@ -1,12 +1,12 @@
 """contains custom convolution layers"""
-from typing import Optional, Tuple, Union
+from typing import Tuple, Union
 
 import torch
 import torch.nn.functional as F
 from torch import nn
 
 
-class DepthWiseConv2d(nn.Module):
+class DepthWiseConv2d(nn.Conv2d):
     def __init__(
         self,
         in_channels: int,
@@ -16,34 +16,22 @@ class DepthWiseConv2d(nn.Module):
         bias: bool = True,
         padding: Union[int, Tuple[int, int]] = 0,
     ):
-        super(DepthWiseConv2d, self).__init__()
-        self.in_channels: int = in_channels
-        self.conv_groups: int = in_channels
-        self.expansion: int = expansion
-        self.out_channels: int = in_channels * expansion
-        self.kernel_size: Tuple[int, int] = kernel_size
-        self.stride: Tuple[int, int] = stride
-        self.bias: Optional[torch.Tensor] = torch.randn(self.out_channels) if bias else None
-        self.padding: Union[int, Tuple[int, int]] = padding
-        self.weight = torch.randn(  # kernel
-            self.out_channels,
-            self.in_channels // self.conv_groups,
-            kernel_size[0],
-            kernel_size[1],
+        super(DepthWiseConv2d, self).__init__(
+            in_channels=in_channels,
+            out_channels=in_channels * expansion,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            groups=in_channels,
+            bias=bias,
         )
+        self.expansion: int = expansion
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        return F.conv2d(
-            inputs,
-            weight=self.weight,
-            bias=self.bias,
-            stride=self.stride,
-            groups=self.conv_groups,
-            padding=self.padding,
-        )
+        return self._conv_forward(inputs, self.weight, self.bias)
 
 
-class PointWiseConv2d(nn.Module):
+class PointWiseConv2d(nn.Conv2d):
     def __init__(
         self,
         in_channels: int,
@@ -51,27 +39,63 @@ class PointWiseConv2d(nn.Module):
         bias: bool = True,
         padding: Union[int, Tuple[int, int]] = 0,
     ):
-        super(PointWiseConv2d, self).__init__()
-        self.in_channels: int = in_channels
-        self.conv_groups: int = 1
-        self.out_channels: int = out_channels
-        self.kernel_size: Tuple[int, int] = (1, 1)
-        self.stride: Tuple[int, int] = (1, 1)
-        self.bias: Optional[torch.Tensor] = torch.randn(self.out_channels) if bias else None
-        self.padding: Union[int, Tuple[int, int]] = padding
-        self.weight = torch.randn(
-            self.out_channels,
-            self.in_channels // self.conv_groups,
-            self.kernel_size[0],
-            self.kernel_size[1],
+        super(PointWiseConv2d, self).__init__(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            padding=padding,
+            bias=bias,
         )
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        return F.conv2d(
-            inputs,
-            weight=self.weight,
-            bias=self.bias,
-            stride=self.stride,
-            groups=self.conv_groups,
-            padding=self.padding,
+        return self._conv_forward(inputs, self.weight, self.bias)
+
+
+class InvertedResBlock(nn.Module):
+    def __init__(
+        self,
+        channels: int,
+        kernel_size: Tuple[int, int] = (3, 3),
+        bias: bool = True,
+        expansion: int = 6,
+    ):
+        super(InvertedResBlock, self).__init__()
+        padding_size = (
+            kernel_size[0] // 2,
+            kernel_size[1] // 2,
         )
+
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.pconv1 = PointWiseConv2d(
+            in_channels=channels,
+            out_channels=channels * expansion,
+            bias=bias,
+        )
+        self.bn2 = nn.BatchNorm2d(self.pconv1.out_channels)
+        self.dconv = DepthWiseConv2d(
+            in_channels=self.pconv1.out_channels,
+            expansion=1,
+            kernel_size=kernel_size,
+            stride=(1, 1),
+            bias=bias,
+            padding=padding_size,
+        )
+        self.bn3 = nn.BatchNorm2d(self.dconv.out_channels)
+        self.pconv2 = PointWiseConv2d(
+            self.dconv.out_channels,
+            channels,
+            bias,
+        )
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = self.bn1(inputs)
+        x = F.relu6(x, inplace=True)
+        x = self.pconv1(x)
+        x = self.bn2(x)
+        x = F.relu6(x, inplace=True)
+        x = self.dconv(x)
+        x = self.bn3(x)
+        x = F.relu6(x, inplace=True)
+        x = self.pconv2(x)
+        res = x + inputs
+        return res
