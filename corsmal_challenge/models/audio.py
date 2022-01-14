1ccb714c49917f5b03e20ae630fc751f2588e113
OtsuKotsu <otoxukk.y@gmail.com>
2022-01-14 18:49:09 +0900

    add V2

--- corsmal_challenge/models/audio.py
+++ corsmal_challenge/models/audio.py
@@ -3,7 +3,11 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from corsmal_challenge.models.convolution import DepthWiseConv2d, PointWiseConv2d
+from corsmal_challenge.models.convolution import (
+    DepthWiseConv2d,
+    PointWiseConv2d,
+    make_inverted_res_stack,
+)
 from corsmal_challenge.models.transformer import TransformerEncoder
 
 
@@ -47,6 +51,51 @@ class LogMelEncoder(nn.Module):
         x: torch.Tensor = self.norm(inputs)
         x = F.relu(x)
         x = self.dconv(x)
+        x = self.pconv(x)
+        x = F.relu(x)
+        x = x.squeeze(dim=1)  # batches, sequence_len, embed_dim
+        x = self.transformer(x)
+        return x
+
+
+class LogMelEncoderV2(nn.Module):
+    def __init__(
+        self,
+        in_channels: int = 8,
+        embed_dim: int = 128,
+        dropout: float = 0.05,
+    ):
+        super(LogMelEncoderV2, self).__init__()
+
+        self.in_channels: int = in_channels
+        self.num_encoder_blocks: int = 2
+        self.embed_dim: int = embed_dim
+        self.num_heads: int = 2
+        self.dropout: float = dropout
+
+        self.norm = nn.BatchNorm2d(in_channels)
+        self.irb = make_inverted_res_stack(in_channels, (3, 3), bias=True, expansion=4, layers=5)
+        self.pconv = PointWiseConv2d(self.in_channels, out_channels=1, bias=True)
+
+        self.transformer = TransformerEncoder(
+            self.num_encoder_blocks,
+            self.embed_dim,
+            self.num_heads,
+            self.dropout,
+        )
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """processes log mel spectrogram data
+
+        Args:
+            inputs (torch.Tensor): (batches, channels, sequence_len, embed_dim)
+
+        Returns:
+            torch.Tensor: (batches, sequence_len, embed_dim)
+        """
+        x: torch.Tensor = self.norm(inputs)
+        x = F.relu(x)
+        x = self.irb(x)
         x = self.pconv(x)
         x = F.relu(x)
         x = x.squeeze(dim=1)  # batches, sequence_len, embed_dim
