1ebb88117f89ce485c673ff7c3695596aafea08f
OtsuKotsu <otoxukk.y@gmail.com>
2022-01-14 18:52:16 +0900

    add T1HeadV2

--- corsmal_challenge/models/task1_2.py
+++ corsmal_challenge/models/task1_2.py
@@ -31,3 +31,21 @@ class T2Head(nn.Module):
         x = F.relu(x)
         x = self.fc2(x)
         return x
+
+
+class T1HeadV2(nn.Module):
+    def __init__(self, embed_dim: int = 128, classify_num: int = 3):
+        super(T1HeadV2, self).__init__()
+        self.classify_num = classify_num
+        self.embed_dim = embed_dim
+        self.fc1 = nn.Linear(embed_dim, 64)
+        self.fc2 = nn.Linear(64, 16)
+        self.fc3 = nn.Linear(16, classify_num)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x: torch.Tensor = self.fc1(inputs)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
